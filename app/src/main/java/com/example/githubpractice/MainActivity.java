d846e1cd015fbcaa0143ca1015612f88332dbcba
Janani <you@example.com>
2022-05-25 18:09:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/githubpractice/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.githubpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Once upon a time, there lived a bear.");
+
+        int bob = 0;
+        for(int i=0; i<5; i++){
+            bob += 1;
+        }
+        System.out.println(bob);
+    }
+}
