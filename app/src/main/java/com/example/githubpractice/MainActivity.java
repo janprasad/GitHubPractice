8f79749ce7c34e5246e0fde157679d105d52a075
amrutha <amruthatummala123@gmail.com>
2022-05-25 19:09:08 -0700

    hello world

--- app/src/main/java/com/example/githubpractice/MainActivity.java
+++ app/src/main/java/com/example/githubpractice/MainActivity.java
@@ -11,17 +11,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Once upon a time, there lived a bear.");
+
 
         int bob = 0;
         for(int i=0; i<5; i++){
             bob += 2;
+            i--;
         }
-        System.out.println(bob);
+
 
         System.out.println("ahhhhh");
         for(int i=0; i<2; i++){
-            i++;
+            i+=2;
+            int x= 4;
         }
 
 
